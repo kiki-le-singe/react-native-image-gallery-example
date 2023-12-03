d599152fee46168285f429b93137b3d42c86335d
anthony <aalbertini95@gmail.com>
2023-12-03 15:03:38 +0100

    init project

---
+++ app/_layout.tsx
@@ -0,0 +1,28 @@
+import { Stack } from 'expo-router';
+
+export default function Layout() {
+  return (
+    <Stack initialRouteName="index">
+      <Stack.Screen
+        name="index"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="card"
+        options={{
+          headerShadowVisible: true,
+          title: 'Images',
+          headerShown: true,
+        }}
+      />
+      <Stack.Screen
+        name="modal"
+        options={{
+          presentation: 'modal',
+        }}
+      />
+    </Stack>
+  );
+}
---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+
+export default function Home() {
+  return (
+    // <View>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text>Home</Text>
+    </View>
+  );
+}
